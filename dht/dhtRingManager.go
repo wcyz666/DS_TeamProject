aedbb338a5482473aa7788eacdd81d8e3cb3b68b
phanishankar <phanishankarpr@gmail.com>
2016-04-06 18:42:08 -0700

    Added fix to include key space for single node

--- dht/dhtRingManager.go
+++ dht/dhtRingManager.go
@@ -88,6 +88,10 @@ func getBigIntFromString(key string) *big.Int{
  * KeyspaceRange is from (previous node's key + 1) to current node's key
 */
 func (dhtNode *DHTNode) isKeyPresentInMyKeyspaceRange(key string) bool {
+	if ((dhtNode.leafTable.prevNode == nil ) && (dhtNode.leafTable.nextNode == nil)){
+		return true
+	}
+	
 	numericKey := getBigIntFromString(key)
 
 	/* If I have to traverse anti-clock wise from my node to reach the key and traverse clock-wise
