88d117f607f3aff39e3a293dbcc32ffa0d592b72
lumig242 <lumig242@gmail.com>
2016-04-09 23:15:33 -0700

    Split stream apis

--- streaming/streamer/streamer.go
+++ streaming/streamer/streamer.go
@@ -2,7 +2,6 @@ package streamer
 
 import (
 	MP "../../messagePasser"
-	"strconv"
 	SDataType "../"
 	NC "../../node/nodeContext"
 	"fmt"
@@ -61,83 +60,7 @@ func (streamer *Streamer) backgroundStreaming(){
 }
 
 
-/*
-The control flow related functions
-*/
 
-/* The request to be the first to start a streming */
-func (streamer *Streamer) Start(title string){
-	fmt.Println("Start to sttreaming a program!")
-	if streamer.STATE != IDEAL{
-		return
-	}
-
-	// Update local program id
-	streamer.streamID += 1
-
-	// Construct data, to be used by updating the program
-	data := SDataType.StreamControlMsg{
-		SrcName: streamer.nodeContext.LocalName,
-		StreamID: streamer.streamID,
-		Title: title,
-	}
-
-	fmt.Println("Sending confirming message " + data + " to the supernode " + streamer.nodeContext.ParentName)
-	// Notify the parent
-	streamer.mp.Send(MP.NewMessage(streamer.nodeContext.ParentIP, streamer.nodeContext.ParentName, "stream_start", MP.EncodeData(data)))
-	streamer.STATE = STREAMING
-}
-
-/* The streamer quit the streaming process */
-func (streamer *Streamer) Stop(){
-	if streamer.STATE != STREAMING{
-		return
-	}
-
-	// Construct data, to be used by updating the program
-	data := SDataType.StreamControlMsg{
-		SrcName: streamer.nodeContext.LocalName,
-		StreamID: streamer.streamID,
-		RootStreamer: streamer.nodeContext.LocalName,
-	}
-
-	// Notify the parent
-	streamer.mp.Send(MP.NewMessage(streamer.nodeContext.ParentIP, streamer.nodeContext.ParentName, "stream_stop", MP.EncodeData(data)))
-
-	// Notify Stream Children
-	streamer.HandleStop(nil)
-
-
-	streamer.STATE = IDEAL
-}
-
-/* A node request to join a certain program */
-func (streamer *Streamer) Join(root string){
-	fmt.Println("Request to join a program! " + root)
-	if streamer.STATE != IDEAL {
-		return
-	}
-
-	// Construct data, to be used by updating the program
-	data := SDataType.StreamControlMsg{
-		SrcName: streamer.nodeContext.LocalName,
-		RootStreamer: root,
-	}
-
-
-	// Notify the parent
-	fmt.Println("Sending confirming message " + data + " to the supernode " + streamer.nodeContext.ParentName)
-	streamer.mp.Send(MP.NewMessage(streamer.nodeContext.ParentIP, streamer.nodeContext.ParentName, "stream_join", MP.EncodeData(data)))
-
-
-	streamer.STATE = JOINING
-}
-
-
-/* Called to stream the data */
-func (streamer *Streamer) Stream(data string){
-	streamer.StreamingData <- data
-}
 
 /********************************************************************************/
 /*   When in the STATE of streaming*/
