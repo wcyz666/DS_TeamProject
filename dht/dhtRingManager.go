60d801383d9f36262a05bf0a47bdacd728539794
phanishankar <phanishankarpr@gmail.com>
2016-04-15 15:14:15 -0700

    added debug logs

--- dht/dhtRingManager.go
+++ dht/dhtRingManager.go
@@ -10,6 +10,7 @@ import (
 	"math/big"
 	"fmt"
 	"time"
+	"strconv"
 )
 
 const (
@@ -341,7 +342,7 @@ func (dhtNode *DHTNode) HandleJoinRes(msg *MP.Message) (int,*Node) {
 /* Apart from periodically refreshing the table, there might be other events where we want to immediately
 *  refresh the table instead of waiting for the timer to exprire. Invoke this method during those cases */
 func (dhtNode *DHTNode) RefreshLeafTable(event int){
-	fmt.Println("Refresh Leaf Table for event " + event)
+	fmt.Println("Refresh Leaf Table for event " + strconv.Itoa(event))
 	if (false == dhtNode.AmITheOnlyNodeInDHT()) {
 
 		//fmt.Println("Triggering Periodic Neighbourhood discovery")
