0c51dbe3cd2ef858d3e2d0dfd31f516550b2298d
lumig242 <lumig242@gmail.com>
2016-04-17 23:01:42 -0700

    Json P

--- node/WebInterface.go
+++ node/WebInterface.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hoisie/web"
 	Streamer "../streaming/streamer"
 	Json "encoding/json"
+	"fmt"
 )
 
 func apiHello()string{
@@ -34,6 +35,7 @@ func apiReceive() string{
 
 func apiGetPrograms(ctx *web.Context, val string) string{
 	json, _ := Json.Marshal(streamer.ProgramList)
+	fmt.Println(ctx.Params["callback"] + "(" + string(json) + ")")
 	return ctx.Params["callback"] + "(" + string(json) + ")"
 
 }
@@ -45,6 +47,6 @@ func webInterface(streamer *Streamer.Streamer) {
 	web.Get("/join/(.*)", apiJoin)
 	web.Get("/stream/(.*)", apiStream)
 	web.Get("/receive/", apiReceive)
-	web.Get("/allPrograms/", apiGetPrograms)
+	web.Get("/allPrograms/(.*)", apiGetPrograms)
 	web.Run("0.0.0.0:9999")
 }
