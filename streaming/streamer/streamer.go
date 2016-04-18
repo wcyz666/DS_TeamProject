cb06f01fcd9f4d8dfaf332cf2edb8fb678c2782f
lumig242 <lumig242@gmail.com>
2016-04-17 22:43:51 -0700

    Cross domain bug.

--- streaming/streamer/streamer.go
+++ streaming/streamer/streamer.go
@@ -33,8 +33,8 @@ Initialization
  */
 func NewStreamer(mp *MP.MessagePasser, nodeContext *NC.NodeContext) *Streamer{
 	streamer := Streamer{STATE:IDEAL, mp: mp, streamID:0, nodeContext:nodeContext}
-	streamer.StreamingData = make(chan string)
-	streamer.ReceivingData = make(chan string)
+	streamer.StreamingData = make(chan string, 1000)
+	streamer.ReceivingData = make(chan string, 1000)
 	streamer.ProgramList = make(map[string]string)
 	go streamer.backgroundStreaming()
 	//go streamer.testReceive()
