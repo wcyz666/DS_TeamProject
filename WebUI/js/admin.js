7b2bf4c56a6dab8513fd1b3d7d2f2c85caa76300
WANG Cheng <wcyz666@126.com>
2016-04-23 12:00:08 -0700

    UI FINALIZED

--- WebUI/js/admin.js
+++ WebUI/js/admin.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         prevData = null,
         refreshButton = $("#btn-refresh"),
         notifArea = $("#notif-area"),
-        graphArea;
+        graphArea,
         utils = {
             trackUsage: function () {
                 var isChanged;
@@ -43,8 +43,8 @@ $(document).ready(function () {
                         html = $("<tr><td>" + data[i].IP + "</td><td>"
                             + data[i].Name + "</td><td>"
                             + data[i].ChildCount + "</td><td>"
-                            + '<button class="btn btn-sm btn-info"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span>' +
-                            '</button></td></tr>');
+                            + '<button class="btn btn-sm" href="#collapse-' + i + '" data-toggle="collapse" aria-expanded="true" class=""><span class="glyphicon glyphicon-minus"></span></button>' +
+                            '</td></tr>');
 
                         tableBody.append(html);
 
@@ -55,7 +55,7 @@ $(document).ready(function () {
                                 detail += "<tr><td>" + content[j].StreamerIp + "</td><td>" + content[j].StreamerName + "</td><td>" +
                                     content[j].StreamProgramName + "</td></tr>"
                             }
-                            utils.getDetailsTemplate().find('tbody').append($(detail)).end().appendTo(tableBody);
+                            utils.getDetailsTemplate(i).find('tbody').append($(detail)).end().hide().appendTo(tableBody).slideDown();
                         } else {
                             html.find('button').addClass('disabled');
                         }
@@ -64,7 +64,7 @@ $(document).ready(function () {
                     refreshButton.removeClass("disabled");
 
                     if (isChanged) {
-                        utils.generateGraph(data, utils.redraw);
+                        graphArea.drawGraph(data);
                     }
 
                 }).error(function (data) {
@@ -130,67 +130,8 @@ $(document).ready(function () {
                 notification.show();
             },
 
-            generateGraph: function (rawData, callback) {
-                var i,
-                    graph = {
-                        "nodes": [],
-                        "edges": []
-                    },
-                    len,
-                    node,
-                    edge,
-                    coordinate;
-
-                for (i = 0, len = rawData.length; i < len; i++) {
-                    node = {
-                        id : "n" + i,
-                        size : 3
-                    };
-
-                    node.label = "IP: " + rawData[i].IP + ", ChildCount: " + rawData[i].ChildCount;
-                    coordinate = utils.getCoordinates(CONST.TOPO_EDGE_LENGTH, i + 1, len);
-                    node.x = coordinate.x;
-                    node.y = coordinate.y;
-
-                    edge = {
-                        id : "e" + i,
-                        source : "n" + i,
-                        target : "n" + ((i + 1) % len)
-                    };
-
-                    graph.nodes.push(node);
-                    graph.edges.push(edge);
-                }
-
-                callback && callback(graph);
-            },
-
-            redraw: function (data) {
-                if (graphArea) {
-                    graphArea.graph.clear();
-                    graphArea.graph.read(data);
-                    graphArea.refresh();
-                } else {
-                    graphArea = new sigma({
-                        graph: data,
-                        container: 'topo-area',
-                        settings: {
-                            defaultNodeColor: '#ec5148'
-                        }
-                    });
-                }
-
-            },
-
-            getDetailsTemplate: function () {
-                return $('<tr><td colspan="4" class="detail-container"><div class="collapsePart"><table class="table table-responsive table-detail"><thead><tr><th>Streamer IP</th><th>Program ID</th><th>Program Title</th></tr></thead><tbody></tbody></table></div></td></tr>');
-            },
-
-            getCoordinates: function (edgeLength, order, edgeCount) {
-                return {
-                    "x" : edgeLength * Math.cos(Math.PI / 2 + (order - 1 ) * Math.PI * 2 / edgeCount),
-                    "y" : edgeLength * Math.sin(Math.PI / 2 + (order - 1 ) * Math.PI * 2 / edgeCount)
-                };
+            getDetailsTemplate: function (id) {
+                return $('<tr id="collapse-' + id + '"><td colspan="4" class="detail-container"><div class="collapsePart"><table class="table table-responsive table-detail"><thead><tr><th>Streamer IP</th><th>Program ID</th><th>Program Title</th></tr></thead><tbody></tbody></table></div></td></tr>');
             }
         };
 
@@ -204,6 +145,70 @@ $(document).ready(function () {
     }
     Notification.prototype.show = function () {
         this.HTML.hide().appendTo(notifArea).slideDown();
+    };
+
+    function GraphArea(container) {
+        this.container = container;
+        this.graphArea = null;
+    }
+
+    GraphArea.prototype.drawGraph = function (rawData) {
+        var i,
+            graph = {
+                "nodes": [],
+                "edges": []
+            },
+            len,
+            node,
+            edge,
+            coordinate;
+
+        for (i = 0, len = rawData.length; i < len; i++) {
+            node = {
+                id : "n" + i,
+                size : 3
+            };
+
+            node.label = "IP: " + rawData[i].IP + ", ChildCount: " + rawData[i].ChildCount;
+            coordinate = this.getCoordinates(CONST.TOPO_EDGE_LENGTH, i + 1, len);
+            node.x = coordinate.x;
+            node.y = coordinate.y;
+
+            edge = {
+                id : "e" + i,
+                source : "n" + i,
+                target : "n" + ((i + 1) % len)
+            };
+
+            graph.nodes.push(node);
+            graph.edges.push(edge);
+        }
+
+        this.redraw(graph);
+    };
+
+    GraphArea.prototype.redraw = function (data) {
+
+        if (this.graphArea) {
+            this.graphArea.graph.clear();
+            this.graphArea.graph.read(data);
+            this.graphArea.refresh();
+        } else {
+            this.graphArea = new sigma({
+                graph: data,
+                container: this.container,
+                settings: {
+                    defaultNodeColor: '#ec5148'
+                }
+            });
+        }
+    };
+
+    GraphArea.prototype.getCoordinates = function (edgeLength, order, edgeCount) {
+        return {
+            "x" : edgeLength * Math.cos(Math.PI / 2 + (order - 1 ) * Math.PI * 2 / edgeCount),
+            "y" : edgeLength * Math.sin(Math.PI / 2 + (order - 1 ) * Math.PI * 2 / edgeCount)
+        };
     };
 
     (function () {
@@ -237,8 +242,8 @@ $(document).ready(function () {
         }
 
         function initElement() {
+            graphArea = new GraphArea("topo-area");
             utils.trackUsage();
-
         }
 
 
